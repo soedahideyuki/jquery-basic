4fe690b65c9c260b9ecd92a09711c62b545ba629
soedahideyuki <kiyudehi811@gmail.com>
2023-11-27 09:56:43 +0900

    関数について、アロー関数は使わない

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-$(function(){ //アロー関数はつかえない？
-  /*const title=$('#title').text();  //質問 getElementByIdはつかえない？
+$(function(){ 
+  // const title=document.getElementById('title').innerHTML;  //質問 getElementByIdはつかえない？
+  const title=$('#title').text();
   const list=$('.message').text();
   console.log(title);
-  console.log(list);*/
+  console.log(list);
   
 //class属性値の先頭が「sample」の要素だけを抽出する
 const text=$('li[class^="sample"]').text();  //　＊部分一致　^前方一致　$後方一致
