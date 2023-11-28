66131459a3f7474392cb214a10cf46154edb9ef0
soedahideyuki <kiyudehi811@gmail.com>
2023-11-28 18:34:21 +0900

    アニメーション２

---
+++ js/animation2.js
@@ -0,0 +1,17 @@
+$(function(){
+  $('#fadeOut').on('click',function(){
+    $('.box').css('opacity',0);
+  });
+  $('#fadeIn').on('click',function(){
+    $('.box').css('opacity',1);
+  });
+  $('#slideUp').on('click',function(){
+    $('.box').css('height',0);
+  });
+  $('#slideDown').on('click',function(){
+    $('.box').css('height',200);
+  });
+  /*$('#slideToggle').on('click',function(){
+    $('.box').slideToggle();
+  });*/
+});
